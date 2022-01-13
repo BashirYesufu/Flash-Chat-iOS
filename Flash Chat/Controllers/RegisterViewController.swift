39303292ccbee80cf56df0b940f333fc22b3cc16
Bashir Yesufu <yesufubashir@gmail.com>
2022-01-13 18:43:10 +0100

    Used alert to handle errors in registration

--- Flash Chat/Controllers/RegisterViewController.swift
+++ Flash Chat/Controllers/RegisterViewController.swift
@@ -18,7 +18,9 @@ class RegisterViewController: UIViewController {
         if let email = emailTextfield.text, let password = passwordTextfield.text {
             Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
                 if let e = error {
-                    print(e)
+                    let alert = UIAlertController(title: "Error", message: e.localizedDescription, preferredStyle: .alert)
+                    alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+                    self.present(alert, animated: true, completion: nil)
                 } else {
                     self.performSegue(withIdentifier: "RegisterToChat", sender: self)
                 }
