3c082c96b30575243d6f8f623ce198fbe0c9869d
Bashir Yesufu <yesufubashir@gmail.com>
2022-01-15 21:12:16 +0100

    Added table views protocol stubs

--- Flash Chat/Controllers/ChatViewController.swift
+++ Flash Chat/Controllers/ChatViewController.swift
@@ -14,10 +14,18 @@ class ChatViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
     
+    var messages: [Message] = [
+        Message(sender: "1@2.com", body: "Hey!"),
+        Message(sender: "a@b.com", body: "Hello!"),
+        Message(sender: "1@2.com", body: "What's up?!")
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.dataSource = self
+        tableView.delegate = self
         title = K.appName
-        navigationController?.navigationBar.prefersLargeTitles = true
         navigationItem.hidesBackButton = true
     }
     
@@ -34,5 +42,19 @@ class ChatViewController: UIViewController {
             self.present(alert, animated: true, completion: nil)
         }
     }
+    
+}
+
+extension ChatViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath)
+        cell.textLabel?.text = "This is a cell"
+        return cell
+    }
+    
     
 }
