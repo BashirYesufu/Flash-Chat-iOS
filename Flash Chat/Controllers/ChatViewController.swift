6db7bd90033e9bbeabe16b9869db58ba8c71c3f7
Bashir Yesufu <yesufubashir@gmail.com>
2022-01-15 21:20:48 +0100

    Removed Table view delegate methods

--- Flash Chat/Controllers/ChatViewController.swift
+++ Flash Chat/Controllers/ChatViewController.swift
@@ -22,9 +22,9 @@ class ChatViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+    
         tableView.dataSource = self
-        tableView.delegate = self
+        
         title = K.appName
         navigationItem.hidesBackButton = true
     }
@@ -45,16 +45,14 @@ class ChatViewController: UIViewController {
     
 }
 
-extension ChatViewController: UITableViewDataSource, UITableViewDelegate {
+extension ChatViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return messages.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath)
-        cell.textLabel?.text = "This is a cell"
+        cell.textLabel?.text = messages[indexPath.row].body
         return cell
     }
-    
-    
 }
